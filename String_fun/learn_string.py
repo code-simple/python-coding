4f443c99109e492d00c26b5cabaef4303060cfac
code-simple <subscribing8@gmail.com>
2022-09-10 23:10:01 +0500

    Updated

--- String_fun/learn_string.py
+++ String_fun/learn_string.py
@@ -1,6 +1,7 @@
 import os
 
 s = "file.exe"
-f_name = s.split('.')[0]
-f_ext = s.split('.')[1]
-print(f_name+'.'+f_ext)
+
+# f_name = s.split('.')[0]
+# f_ext = s.split('.')[1]
+# print(f_name+'.'+f_ext)
