8de5bafd0cac7a4fc014da6338453a4c9f4653e2
code-simple <subscribing8@gmail.com>
2022-09-12 13:00:06 +0500

    13:00   12-Sep-2022

---
+++ pandas/pandas_read.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+csv_file = "/home/administrator/Desktop/python-folder/pandas/sample.csv"
+
+# l = pd.read_csv(csv_file,header=0).values.tolist()
+l = pd.read_csv(csv_file,header=0).to_numpy()
+
+
+name_list = []
+age_list = []
+state_list = []
+point_list = []
+
+for i in l:
+    name_list.append(i[0])
+    age_list.append(i[1])
+    state_list.append(i[2])
+    point_list.append(i[3])
+
+print(state_list)
