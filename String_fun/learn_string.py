484438315d80b586e6b4e348bded01d742023de1
code-simple <subscribing8@gmail.com>
2022-09-17 17:48:55 +0500

    str to float, with comma

--- String_fun/learn_string.py
+++ String_fun/learn_string.py
@@ -1,8 +1,15 @@
 import os
 
-s = "file.exe"
+# s = "file.exe"
 
-f_name = s.split('.')[0]
-f_ext = s.split('.')[1]
-print(f_name+'.'+f_ext)
-print("FULL FILE NAME")
+# f_name = s.split('.')[0]
+# f_ext = s.split('.')[1]
+# print(f_name+'.'+f_ext)
+# print("Working in Nano")
+
+
+# Convert any string to show only 2 decimal 
+# First convert to float, if it has comma replace it
+# 
+gold_price = "1,680.5345"
+print(f"XAUUSD : {round(float(gold_price.replace(',', '')),2)}")
--- gold.py
+++ gold.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-webpage = requests.get("https://www.cnbc.com/quotes/XAU=")
-soup = BeautifulSoup(webpage.content,'lxml')
-price = soup.select_one(".QuoteStrip-lastPrice").text.strip()
-print(price)
+gold = requests.get("https://www.cnbc.com/quotes/XAU=")
+gold_soup = BeautifulSoup(gold.content,'lxml')
+gold_price = gold_soup.select_one(".QuoteStrip-lastPrice").text.strip()
+
+oil = requests.get("https://www.cnbc.com/quotes/@CL.1")
+oil_soup = BeautifulSoup(oil.content,'lxml')
+oil_price = oil_soup.select_one(".QuoteStrip-lastPrice").text.strip()
+
+print("XAUUSD : "+gold_price+"\nUSLOIL : "+oil_price)
