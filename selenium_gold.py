646182571e0abd57ee8c110fa493c1845880686d
code-simple <subscribing8@gmail.com>
2022-10-05 04:22:31 +0500

    No Need BS4

--- selenium_gold.py
+++ selenium_gold.py
@@ -1,8 +1,10 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 def goldPrice():
     options = Options()
@@ -10,8 +12,9 @@ def goldPrice():
     s=Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=s,options=options)
     content = driver.get("https://www.tradingview.com/symbols/XAUUSD/")
-    soup = BeautifulSoup(driver.page_source,'lxml')
-    price = soup.select_one(".tv-symbol-price-quote__value").text.strip()
+    price = driver.find_element(By.CLASS_NAME, "tv-symbol-price-quote__value").text.strip()
+    # soup = BeautifulSoup(driver.page_source,'lxml')
+    # price = soup.select_one(".tv-symbol-price-quote__value").text.strip()
     driver.close()
     return price
 
@@ -21,8 +24,9 @@ def oilPrice():
     s=Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=s,options=options)
     content = driver.get("https://www.tradingview.com/symbols/FX-USOILSPOT/")
-    soup = BeautifulSoup(driver.page_source,'lxml')
-    price = soup.select_one(".tv-symbol-price-quote__value").text.strip()
+    price = driver.find_element(By.CLASS_NAME, "tv-symbol-price-quote__value").text.strip()
+    # soup = BeautifulSoup(driver.page_source,'lxml')
+    # price = soup.select_one(".tv-symbol-price-quote__value").text.strip()
     driver.close()
     return price
 
